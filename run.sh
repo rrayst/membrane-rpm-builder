fc103be42b2d1f475a3ae2c7c75b877f058c5360
Tobias Polley <polley@predic8.de>
2024-06-20 07:34:07 +0200

    some more work

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-jq --help
+VERSION="$1"
+SSH_PUB_KEY="$2"
 
-az --help
+if [ "$VERSION" = "" -o "$SSH_PUB_KEY" = "" ] ; then
+	echo "Parameters: $0 <version> <ssh-public-key>"
+	exit 1
+fi
 
-jq -Rs --arg SSH_PUB_KEY Tob --arg "$(cat starter-complete.sh)" cust "$(cat myparameters.json)" </dev/null
 
-
-exit 0
+jq -Rs \
+--arg SSH_PUB_KEY "$SSH_PUB_KEY" \
+--arg CUSTOM_DATA "$(cat starter-complete.sh)" \
+--arg USER_DATA "$(echo $VERSION | base64)" \
+"$(cat myparameters.json)" \
+</dev/null
 
 az deployment group create --name addnameparameter --resource-group demo --template-file .\template.json --parameters "@myparameters.json" > data.json
 
