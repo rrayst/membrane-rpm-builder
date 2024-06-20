c0240f38bac1d2c6e03fc130c46143eef20bbe10
Tobias Polley <polley@predic8.de>
2024-06-20 08:10:38 +0200

    adjusted template

--- run.sh
+++ run.sh
@@ -17,8 +17,3 @@ jq -Rs \
 >myparameters2.json </dev/null
 
 az deployment group create --name addnameparameter --resource-group demo --template-file template.json --parameters "@myparameters2.json" > data.json
-
-
-MANAGED_ID=$(cat data.json | jq -r .identity.principalId)
-
-az role assignment create --role "Key Vault Secrets User" --assignee "$MANAGED_ID" --scope /subscriptions/2a5e9a0c-6d1d-49f6-8b2a-96647a511370/resourcegroups/demo/providers/Microsoft.KeyVault/vaults/rpm-builder
